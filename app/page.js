e70b1621ea89b7305dfbddd851e2c7c1e5712b74
preshitpawar-beep <preshitpawar@gmail.com>
2025-12-18 14:57:20 +0000

    Replace Hero component with HeroSlider
    Add auto hero slider

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import Hero from "../components/Hero";
+import HeroSlider from "../components/HeroSlider";
 import Slider from "../components/Slider";
 import FeaturedProducts from "../components/FeaturedProducts";
 import Categories from "../components/Categories";
