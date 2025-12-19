486593f37b6acb124287a92d6967c67ae2d2875d
preshitpawar-beep <preshitpawar@gmail.com>
2025-12-19 09:18:31 +0000

    Replace Categories component with Benefits component

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import HeroSlider from "../components/HeroSlider";
 import TrustIntro from "../components/TrustIntro";
 import Slider from "../components/Slider";
 import FeaturedProducts from "../components/FeaturedProducts";
-import Categories from "../components/Categories";
+import Benefits from "../components/Benefits";
 import WhyChoose from "../components/WhyChoose";
 import HowItWorks from "../components/HowItWorks";
 import CTA from "../components/CTA";
@@ -14,7 +14,7 @@ export default function Home() {
       <TrustIntro />
       <Slider />
       <FeaturedProducts />
-      <Categories />
+      <Benefits />
       <WhyChoose />
       <HowItWorks />
       <CTA />
