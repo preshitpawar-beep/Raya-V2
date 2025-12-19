f8f562bbca2c9e3209377fb8be35f3d92fb9ddbb
preshitpawar-beep <preshitpawar@gmail.com>
2025-12-19 11:37:20 +0000

    Update HeroSlider.js

--- components/HeroSlider.js
+++ components/HeroSlider.js
@@ -55,7 +55,7 @@ export default function HeroSlider() {
             initial={{ opacity: 0, scale: 0.98 }}
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0.98 }}
-            transition={{ duration: 0.8, ease: "easeInOut" }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
             className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-12 items-center"
           >
             {/* TEXT */}
