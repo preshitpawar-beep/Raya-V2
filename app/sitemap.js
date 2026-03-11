a9ab58d3c8d7a41f111b52a9a162222237bb6bcc
preshitpawar-beep <preshitpawar@gmail.com>
2026-03-11 15:42:49 +0000

    Add sitemap function for site URLs

---
+++ app/sitemap.js
@@ -0,0 +1,36 @@
+export default function sitemap() {
+  const baseUrl = "https://www.legacyimprint.co.uk";
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/products`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/quote`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/privacy-policy`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.3,
+    },
+    {
+      url: `${baseUrl}/terms`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.3,
+    },
+  ];
+}
